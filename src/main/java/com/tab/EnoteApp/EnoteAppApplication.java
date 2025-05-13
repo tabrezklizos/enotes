ed97613984cd0f0baf56efd7e05415fc4f7566f2
tabrezklizos <Tabrez@klizos.com>
2025-05-13 16:24:14 +0530

    initial commit

---
+++ src/main/java/com/tab/EnoteApp/EnoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.tab.EnoteApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoteAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tab/EnoteApp/EnoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tab.EnoteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
