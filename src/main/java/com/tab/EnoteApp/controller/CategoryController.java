255c0c94fad28ae82201a77ab5c614da5efd4d25
MD TABREZ ALAM <tabrez@klizos.com>
2025-05-16 13:13:28 +0530

    categrory controller added

--- src/main/java/com/tab/EnoteApp/controller/CategoryController.java
+++ src/main/java/com/tab/EnoteApp/controller/CategoryController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/category")
-public class CategoryController {
+public class    CategoryController {
 
     @Autowired
     CategoryService categoryService;
