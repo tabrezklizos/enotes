817810ac5bed771f6c4aec7d1639412e457fa3d7
MD TABREZ ALAM <tabrez@klizos.com>
2025-05-21 13:42:04 +0530

    validation class

---
+++ src/main/java/com/tab/EnoteApp/util/Validation.java
@@ -0,0 +1,58 @@
+package com.tab.EnoteApp.util;
+
+import com.tab.EnoteApp.dto.CategoryDto;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Component
+public class Validation {
+
+    public void categoryValidation(CategoryDto categoryDto){
+
+        Map<String,Object> error = new LinkedHashMap<>();
+
+        if(ObjectUtils.isEmpty(categoryDto)){
+            throw new IllegalArgumentException("category or json data is not present");
+        }
+        else{
+
+            if(ObjectUtils.isEmpty(categoryDto.getName())){
+                error.put("name","name is empty or null");
+            }else{
+
+                if(categoryDto.getName().length()<10){
+                    error.put("name","name atleast be 10 char");
+                }
+                if(categoryDto.getName().length()>100){
+                    error.put("name","name atmost be 100 char");
+                }
+            }
+
+            if(ObjectUtils.isEmpty(categoryDto.getIsActive())){
+                error.put("isActive","description is empty or null");
+            }
+            else{
+
+                if(categoryDto.getIsActive() != Boolean.TRUE.booleanValue()
+                        && categoryDto.getIsActive() != Boolean.FALSE.booleanValue()){
+                    error.put("isActive","invalid isActive field");
+                }
+            }
+            if(ObjectUtils.isEmpty(categoryDto.getDescription())){
+                error.put("description","description is empty or null");
+            }
+
+        }
+
+        if(!error.isEmpty()){
+            throw new IllegalArgumentException(error);
+        }
+
+
+    }
+
+
+}
